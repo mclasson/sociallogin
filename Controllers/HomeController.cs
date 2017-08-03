b0b0587ad52dd082de2c9627e275dffb79b14cad
Marcus Classon (Stratiteq) <mclasson@stratiteq.com>
2017-08-03 16:10:03 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace customlogin.Controllers
+{
+            [Authorize]
+    public class HomeController : Controller
+    {
+        readonly SignInManager<ClaimsIdentity> _signInManager = null;
+        public HomeController()
+        {
+         
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            
+            var principal = User.Identity as ClaimsIdentity;
+            var idClaim = principal.Claims.Where(i => i.Type == "http://marcusclasson.com/claims/id").SingleOrDefault();
+            if(idClaim == null)
+            {
+                
+                principal.AddClaim(new Claim("http://marcusclasson.com/claims/id", "MyCustomId"));
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(User);
+            }
+            return View();
+        }
+
+ 
+        public async Task<IActionResult> Logout()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+            var user = HttpContext.User.Identity.Name;
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index");
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (username == "kalle")
+            {
+                        var claims = new List<Claim>(2);
+        claims.Add(new Claim(ClaimTypes.Name, username));
+
+        claims.Add(new Claim(ClaimTypes.Role, "GroupThatUserIsIn", 
+            ClaimValueTypes.String, "IHaveIssuedThis"));
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme)));
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customlogin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookieAuthentication(CookieAuthenticationDefaults.AuthenticationScheme, option =>
+            {
+                option.LoginPath = "/home/login";
+            })
+            .AddTwitterAuthentication(o =>
+            {
+                o.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+                o.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+
+            });
+            
+            services.AddMvc();
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var options = new RewriteOptions().AddRedirectToHttps(301, 44326);
+            app.UseRewriter(options);
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Map("/login-twitter", login =>
+             {
+                 login.Run(async context =>
+                 {
+                     
+                     await context.ChallengeAsync("Twitter", new AuthenticationProperties() {
+                         RedirectUri = "/" });
+                     return;
+                 });
+             });
+
+
+        }
+    }
+}
